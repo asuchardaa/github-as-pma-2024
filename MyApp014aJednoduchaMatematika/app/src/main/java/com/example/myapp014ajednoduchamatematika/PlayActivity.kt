9b29342779fab2a65b5155f64b594ab263cbd770
Adam <adam.sucharda@tul.cz>
2024-11-07 14:28:21 +0100

    #14 fixed and gtg

---
+++ MyApp014aJednoduchaMatematika/app/src/main/java/com/example/myapp014ajednoduchamatematika/PlayActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myapp014ajednoduchamatematika
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_play)
+
+        val addition = findViewById<ImageView>(R.id.addition)
+        val sub = findViewById<ImageView>(R.id.sub)
+        val multi = findViewById<ImageView>(R.id.multi)
+        val division = findViewById<ImageView>(R.id.division)
+
+        addition.setOnClickListener {
+            val calInt = Intent(this@PlayActivity,MainActivity::class.java)
+            calInt.putExtra("cals", "+")
+            startActivity(calInt)
+        }
+
+        sub.setOnClickListener {
+            val calInt = Intent(this@PlayActivity,MainActivity::class.java)
+            calInt.putExtra("cals", "-")
+            startActivity(calInt)
+        }
+
+        multi.setOnClickListener {
+            val calInt = Intent(this@PlayActivity,MainActivity::class.java)
+            calInt.putExtra("cals", "*")
+            startActivity(calInt)
+        }
+
+        division.setOnClickListener {
+            val calInt = Intent(this@PlayActivity,MainActivity::class.java)
+            calInt.putExtra("cals", "/")
+            startActivity(calInt)
+        }
+
+
+    }
+}
